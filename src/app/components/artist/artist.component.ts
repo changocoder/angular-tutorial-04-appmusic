2cd9ee3e06b84b7a4019aded4a6965b08af81529
hugo <hugogher@gmail.com>
2020-08-21 09:01:42 -0300

    initial settings

---
+++ src/app/components/artist/artist.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist',
+  template: `
+    <p>
+      artist works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ArtistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
